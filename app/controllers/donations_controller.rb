f75102c14f28caf6560c8f966098239c68fbedda
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-02 16:13:33 +0900

    寄付情報の保存

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -5,4 +5,20 @@ class DonationsController < ApplicationController
 
   def new
   end
+
+  def create
+    @donation = Donation.create(donation_params)
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def donation_params
+    params.permit(:price).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture, :city, :house_number, :building_name).merge(donation_id: @donation.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "donations#index"
-  resources :donations, only: [:index, :new]
+  resources :donations, only: [:index, :new, :create]
 end
